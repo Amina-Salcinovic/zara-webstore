33b9387b106359e7e84e26fee70fb4711448bc21
Amina-Salcinovic <amina.salcinovic@stu.ibu.edu.ba>
2025-10-15 15:39:50 +0200

    milesotne 1

---
+++ frontend/assets/js/router.js
@@ -0,0 +1,12 @@
+var app = $.spapp({
+    defaultView  : "#shop",
+    templateDir  : "../../pages/"
+  });
+
+// app.route({ view: 'cart', load: 'cart.html'}); 
+// app.route({ view: 'checkout', load: 'checkout.html'}); 
+// app.route({ view: 'contact', load: 'contact.html'}); 
+// app.route({ view: 'detail', load: 'detail.html'}); 
+// app.route({ view: 'shop', load: 'shop.html'}); 
+
+app.run(); 
